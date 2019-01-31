f0d0cc33a63141b966c378616124bf2c3f823b7a
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-01-31 15:33:53 +0100

    test: take care of the coverage for class Json2ApplicationConverter

---
+++ bricks-assemble/src/test/java/com/github/ingogriebsch/bricks/assemble/converter/json/Json2ApplicationConverterTest.java
@@ -0,0 +1,52 @@
+package com.github.ingogriebsch.bricks.assemble.converter.json;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.ingogriebsch.bricks.model.Application;
+
+public class Json2ApplicationConverterTest {
+
+    @Test(expected = NullPointerException.class)
+    public void convert_should_throw_exception_if_input_is_null() throws Exception {
+        new Json2ApplicationConverter().convert(null);
+    }
+
+    @Test(expected = IOException.class)
+    public void convert_should_throw_exception_if_input_is_not_legal() throws Exception {
+        try (InputStream is = new ByteArrayInputStream("test".getBytes())) {
+            new Json2ApplicationConverter().convert(is);
+        }
+    }
+
+    @Test
+    public void convert_should_convert_empty_application_to_matching_output() throws Exception {
+        Application input = new Application();
+
+        Application output;
+        try (InputStream is = new ByteArrayInputStream(new ObjectMapper().writeValueAsBytes(input))) {
+            output = new Json2ApplicationConverter().convert(is);
+        }
+
+        assertThat(output).isNotNull().isEqualTo(input);
+    }
+
+    @Test
+    public void convert_should_convert_filled_application_to_matching_output() throws Exception {
+        Application input = new Application("id", "name", "description", "version", newHashSet());
+
+        Application output;
+        try (InputStream is = new ByteArrayInputStream(new ObjectMapper().writeValueAsBytes(input))) {
+            output = new Json2ApplicationConverter().convert(is);
+        }
+
+        assertThat(output).isNotNull().isEqualTo(input);
+    }
+}
