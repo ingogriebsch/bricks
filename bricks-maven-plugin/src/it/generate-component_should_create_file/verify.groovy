fa21b39080b8e1c56699ea4cda58af7c4c5ab352
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-02-22 14:35:07 +0100

    test: enhance integration test execution verification

--- bricks-maven-plugin/src/it/generate-component_should_create_file/verify.groovy
+++ bricks-maven-plugin/src/it/generate-component_should_create_file/verify.groovy
@@ -1,2 +1,7 @@
-File touchFile = new File( basedir, "target/bricks.json" );
-assert touchFile.isFile()
+import static org.junit.Assert.assertTrue
+
+def outputFilename = "target/bricks.json";
+def outputFile = new File(basedir, outputFilename);
+
+assertTrue "File '" + outputFile.getAbsoluteFile() + "' does not exist!", outputFile.exists()
+assertTrue "File '" + outputFile.getAbsoluteFile() + "' is not a file!", outputFile.isFile()
