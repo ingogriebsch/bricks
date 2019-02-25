f731bca6af29ac5b310b7ecf39968574d026cbc4
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-02-25 20:36:52 +0100

    refactor: define random port as application arg for ctx related test

--- bricks-dashboard/src/test/java/com/github/ingogriebsch/bricks/dashboard/ServiceApplicationTest.java
+++ bricks-dashboard/src/test/java/com/github/ingogriebsch/bricks/dashboard/ServiceApplicationTest.java
@@ -1,12 +1,9 @@
 /*
  * Copyright © 2018 Ingo Griebsch (https://ingogriebsch.de/)
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,9 +23,6 @@ public class ServiceApplicationTest {
 
     @Test
     public void main_should_load_context() {
-        // prevent conflict on port binding
-        System.setProperty("server.port", Integer.valueOf(0).toString());
-        
-        main(new String[] {});
+        main(new String[] { "--server.port=0" });
     }
 }
