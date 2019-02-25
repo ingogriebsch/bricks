b6c2415bdfa9be078567302ef0a9644168a56546
Uwe Schaefer <uweschaefer@users.noreply.github.com>
2019-02-25 20:25:57 +0100

    chore: enhance build process regarding licenses, formatting, sort order
    * added bin and made a few exclusions recursive
    
    * set port in test to 0 (programmatically, probably not the best, but for now the least intrusive way to do that)
    
    * moved lombok config to parent project
    
    * license plugin added
    
    * unify license file headers
    
    * disable automatic updating and replace with checking
    
    * prepared formatter plugin
    
    * renamed formatter config file
    
    * added impsort - feel free to automate sorting instead of just checking
    
    * lifecycle mapping fix for m2e

--- bricks-dashboard/src/test/java/com/github/ingogriebsch/bricks/dashboard/ServiceApplicationTest.java
+++ bricks-dashboard/src/test/java/com/github/ingogriebsch/bricks/dashboard/ServiceApplicationTest.java
@@ -1,11 +1,17 @@
 /*
- * Copyright 2019 Ingo Griebsch
+ * Copyright © 2018 Ingo Griebsch (https://ingogriebsch.de/)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.github.ingogriebsch.bricks.dashboard;
 
@@ -20,6 +26,9 @@ public class ServiceApplicationTest {
 
     @Test
     public void main_should_load_context() {
+        // prevent conflict on port binding
+        System.setProperty("server.port", Integer.valueOf(0).toString());
+        
         main(new String[] {});
     }
 }
