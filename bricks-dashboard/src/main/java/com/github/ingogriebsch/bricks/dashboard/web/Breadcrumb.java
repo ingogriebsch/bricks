28301a198827fcf778abaf633e132eb35eb4d3d3
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-02-27 17:26:46 +0100

    refactor: validate breadcrumb related properties

--- bricks-dashboard/src/main/java/com/github/ingogriebsch/bricks/dashboard/web/Breadcrumb.java
+++ bricks-dashboard/src/main/java/com/github/ingogriebsch/bricks/dashboard/web/Breadcrumb.java
@@ -1,8 +1,28 @@
+/*-
+ * #%L
+ * Bricks Dashboard
+ * %%
+ * Copyright (C) 2018 - 2019 Ingo Griebsch
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.github.ingogriebsch.bricks.dashboard.web;
 
 import java.util.List;
 
 import lombok.Builder;
+import lombok.NonNull;
 import lombok.Singular;
 import lombok.Value;
 
@@ -11,13 +31,16 @@ import lombok.Value;
 public class Breadcrumb {
 
     @Singular
+    @NonNull
     private List<Entry> entries;
 
     @Builder
     @Value
     public static class Entry {
 
+        @NonNull
         private String name;
+        @NonNull
         private String href;
     }
 }
