62719b9cd63f1d3cf4a01b3c696c8c30cfedcecb
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-03-14 20:02:34 +0100

    feat: introduce web layer related exception handler

---
+++ bricks-dashboard/src/main/java/com/github/ingogriebsch/bricks/dashboard/web/ExceptionHandler.java
@@ -0,0 +1,35 @@
+/*-
+ * #%L
+ * Bricks Dashboard
+ * %%
+ * Copyright (C) 2018 - 2019 Ingo Griebsch
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.ingogriebsch.bricks.dashboard.web;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import lombok.NonNull;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+    static final String PAGE_ERROR = "/error";
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalStateException.class)
+    public String illegalStateException(@NonNull IllegalStateException exception) {
+        return PAGE_ERROR;
+    }
+}
