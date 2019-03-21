e8ab08b3111abee1523c0ab403b4fb6997a397f6
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-03-21 19:46:12 +0100

    refactor: use yaml extracting id collectors

--- bricks-dashboard/src/main/java/com/github/ingogriebsch/bricks/dashboard/ServiceConfiguration.java
+++ bricks-dashboard/src/main/java/com/github/ingogriebsch/bricks/dashboard/ServiceConfiguration.java
@@ -19,15 +19,17 @@
  */
 package com.github.ingogriebsch.bricks.dashboard;
 
-import java.util.Map;
-import java.util.Set;
+import java.io.InputStream;
 
 import com.github.ingogriebsch.bricks.assemble.collector.ApplicationCollector;
 import com.github.ingogriebsch.bricks.assemble.collector.ApplicationIdCollector;
 import com.github.ingogriebsch.bricks.assemble.collector.ComponentCollector;
 import com.github.ingogriebsch.bricks.assemble.collector.ComponentIdCollector;
-import com.github.ingogriebsch.bricks.assemble.collector.common.StaticApplicationIdCollector;
-import com.github.ingogriebsch.bricks.assemble.collector.common.StaticComponentIdCollector;
+import com.github.ingogriebsch.bricks.assemble.collector.yaml.ApplicationIdOrigin;
+import com.github.ingogriebsch.bricks.assemble.collector.yaml.ComponentIdOrigin;
+import com.github.ingogriebsch.bricks.assemble.collector.yaml.YamlExtractingApplicationIdCollector;
+import com.github.ingogriebsch.bricks.assemble.collector.yaml.YamlExtractingComponentIdCollector;
+import com.github.ingogriebsch.bricks.assemble.collector.yaml.YamlResourceLoader;
 import com.github.ingogriebsch.bricks.assemble.converter.yaml.Yaml2ApplicationConverter;
 import com.github.ingogriebsch.bricks.assemble.converter.yaml.Yaml2ComponentConverter;
 import com.github.ingogriebsch.bricks.assemble.loader.ResourceLoader;
@@ -40,18 +42,22 @@ import com.github.ingogriebsch.bricks.assemble.reader.common.SimpleApplicationRe
 import com.github.ingogriebsch.bricks.assemble.reader.common.SimpleComponentReader;
 import com.github.ingogriebsch.bricks.assemble.reader.common.StaticApplicationReaderFactory;
 import com.github.ingogriebsch.bricks.assemble.reader.common.StaticComponentReaderFactory;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
 @Configuration
 @RequiredArgsConstructor
 public class ServiceConfiguration {
+
+    private static final String SAMPLE_APPLICATIONS_LOCATION = "classpath:/example-applications.yml";
+    private static final String[] YAML_PATH_APPLICATIONS = new String[] { "bricks", "dashboard", "applications" };
+    private static final String[] YAML_PATH_COMPONENTS = new String[] { "bricks", "dashboard", "applications", "components" };
+    private static final String YAML_KEY_ID = "id";
 
     @NonNull
     private final org.springframework.core.io.ResourceLoader resourceLoader;
@@ -67,27 +73,43 @@ public class ServiceConfiguration {
     }
 
     private ApplicationIdCollector applicationIdCollector() {
-        return new StaticApplicationIdCollector(Sets.newHashSet("sample-app"));
+        return new YamlExtractingApplicationIdCollector(yamlResourceLoader(), applicationIdOrigin());
     }
 
     private ComponentIdCollector componentIdCollector() {
-        Map<String, Set<String>> componentIds = Maps.newHashMap();
-        componentIds.put("sample-app", Sets.newHashSet("component1", "component2"));
-        return new StaticComponentIdCollector(componentIds);
+        return new YamlExtractingComponentIdCollector(yamlResourceLoader(), componentIdOrigin());
     }
 
     private ApplicationReader applicationReader() {
-        return new SimpleApplicationReader(resourceLoader("bricks", "dashboard", "applications"),
-            new Yaml2ApplicationConverter());
+        return new SimpleApplicationReader(resourceLoader(YAML_PATH_APPLICATIONS), new Yaml2ApplicationConverter());
     }
 
     private ComponentReader componentReader() {
-        return new SimpleComponentReader(resourceLoader("bricks", "dashboard", "applications", "components"),
-            new Yaml2ComponentConverter());
+        return new SimpleComponentReader(resourceLoader(YAML_PATH_COMPONENTS), new Yaml2ComponentConverter());
     }
 
     private ResourceLoader resourceLoader(String... parents) {
         return new YamlExtractingResourceLoader(new SpringResourceBasedResourceLoader(resourceLoader,
-            new StaticResourceLocationProvider("classpath:/example-applications.yml")), "id", parents);
+            new StaticResourceLocationProvider(SAMPLE_APPLICATIONS_LOCATION)), YAML_KEY_ID, parents);
     }
+
+    private ApplicationIdOrigin applicationIdOrigin() {
+        return ApplicationIdOrigin.builder().parents(YAML_PATH_APPLICATIONS).build();
+    }
+
+    private ComponentIdOrigin componentIdOrigin() {
+        return ComponentIdOrigin.builder().applicationIdOrigin(applicationIdOrigin()).build();
+    }
+
+    private YamlResourceLoader yamlResourceLoader() {
+        return new YamlResourceLoader() {
+
+            @Override
+            @SneakyThrows
+            public InputStream load() {
+                return resourceLoader.getResource(SAMPLE_APPLICATIONS_LOCATION).getInputStream();
+            }
+        };
+    }
+
 }
