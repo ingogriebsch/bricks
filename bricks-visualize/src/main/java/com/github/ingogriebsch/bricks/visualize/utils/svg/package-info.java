e0aab503778ec247cd62ba5b661a460db45fbf14
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-03-24 18:11:37 +0100

    feat: add package-info containing some helpful links

---
+++ bricks-visualize/src/main/java/com/github/ingogriebsch/bricks/visualize/utils/svg/package-info.java
@@ -0,0 +1,32 @@
+/*-
+ * #%L
+ * Bricks Model
+ * %%
+ * Copyright (C) 2018 - 2019 Ingo Griebsch
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/**
+ * Some helpful links:
+ * <ul>
+ * <li>https://xmlgraphics.apache.org/batik/using/dom-api.html</li>
+ * <li>https://www.w3.org/TR/SVG11/</li>
+ * <li>https://wiki.selfhtml.org/wiki/SVG</li>
+ * <li>https://www.w3schools.com/graphics/svg_intro.asp</li>
+ * <li>https://css-tricks.com/scale-svg/</li>
+ * <li>https://www.mathopenref.com/coordpolycalc.html</li>
+ * <li>https://stackoverflow.com/questions/43092161/how-can-i-get-the-grid-coordinates-of-a-hexagon-tile-by-a-pixel-point-in-java</li>
+ * </ul>
+ */
+package com.github.ingogriebsch.bricks.visualize.utils.svg;
